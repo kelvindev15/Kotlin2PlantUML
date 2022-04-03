2addabca1a26af5aa441e1e939a5daabebe118e2
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-04-03 23:17:39 +0200

    refactor: remove space

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     kotlin("jvm") version "1.6.10"
     id("org.danilopianini.gradle-kotlin-qa") version "0.13.0"
-    id ("org.danilopianini.publish-on-central") version "0.7.15"
+    id("org.danilopianini.publish-on-central") version "0.7.15"
     id("org.jetbrains.dokka") version "1.6.10"
     java
     application
