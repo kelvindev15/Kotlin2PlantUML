1b8809b8f6cfde02fa4b56888c368424fbeeb9cb
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2022-04-16 17:56:28 +0200

    ci: add release config

---
+++ release.config.js
@@ -0,0 +1,20 @@
+var publishCmd = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
+git push --force origin \${nextRelease.version} || exit 6
+./gradlew shadowJar --parallel || ./gradlew shadowJar --parallel || exit 4
+./gradlew releaseKotlinMavenOnMavenCentralNexus --parallel || exit 5
+./gradlew publishKotlinMavenPublicationToGithubRepository --continue || true
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "publishCmd": publishCmd,
+    }],
+    ["@semantic-release/github", {
+        "assets": [
+            { "path": "build/libs/*.jar" },
+        ]
+    }],
+    "@semantic-release/git",
+)
+module.exports = config
