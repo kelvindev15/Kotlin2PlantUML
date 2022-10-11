02fb82d70071258b4c3fb5738b36155343f9ebc3
Kelvin Olaiya <73821880+kelvin-olaiya@users.noreply.github.com>
2022-10-11 18:14:01 +0200

    ci: fix release commands (#115)

--- release.config.js
+++ release.config.js
@@ -2,7 +2,8 @@ var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version} || exit 6
 ./gradlew shadowJar --parallel || ./gradlew shadowJar --parallel || exit 4
-./gradlew uploadAllPublicationsToMavenCentralNexus releaseStagingRepositoryOnMavenCentral
+./gradlew uploadAllPublicationsToMavenCentralNexus releaseStagingRepositoryOnMavenCentral --parallel || exit 5
+./gradlew publishAllPublicationsToGitHubRepository --continue || true
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
